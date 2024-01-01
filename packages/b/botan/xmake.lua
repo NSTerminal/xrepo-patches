b07492f5563b229a95b3f597ff0d60e86d8bc3f9
Aidan Sun <aidansun05@gmail.com>
2024-01-01 01:01:04 -0500

    Add Botan

---
+++ packages/b/botan/xmake.lua
@@ -0,0 +1,44 @@
+package("botan")
+    set_homepage("https://botan.randombit.net")
+    set_description("C++ cryptography library released under the permissive Simplified BSD license")
+    set_license("BSD-2-Clause")
+
+    add_urls("https://botan.randombit.net/releases/Botan-$(version).tar.xz")
+    add_versions("3.2.0", "049c847835fcf6ef3a9e206b33de05dd38999c325e247482772a5598d9e5ece3")
+    add_deps("python", "ninja")
+
+    if is_plat("macosx") then
+        add_frameworks("CoreFoundation", "Security")
+    end
+
+    add_links("botan-3")
+
+    on_install("windows", "macosx", "linux", function (package)
+        local bindir = package:installdir("bin")
+        local libdir = package:installdir("lib")
+
+        if package:is_plat("windows") then
+            import("core.tool.toolchain")
+            local msvc = toolchain.load("msvc", {plat = "windows", arch = os.arch()})
+            local runenvs = msvc:runenvs()
+            os.vrun("python configure.py --build-targets=shared --cc=msvc --build-tool=ninja", { envs = runenvs })
+            import("package.tools.ninja").build(package, {}, { envs = runenvs })
+            os.cp("botan-3.dll", bindir)
+            os.cp("botan-3.lib", libdir)
+        else
+            os.vrun("python configure.py --build-targets=shared --cc=clang --build-tool=ninja")
+            import("package.tools.make").build(package)
+
+            if package:is_plat("macosx") then
+                os.cp("*.dylib", libdir)
+            else
+                os.cp("*.so*", libdir)
+            end
+        end
+
+        os.cp("build/include/(**.h)", package:installdir("include"))
+    end)
+
+    on_test(function (package)
+        assert(package:has_cxxtypes("Botan::TLS::Client", {includes = "botan/tls_client.h"}))
+    end)
