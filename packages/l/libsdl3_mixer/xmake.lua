9e4fb9bb6f479cb634797303e12ce03e3a4c4af5
Mr-L-oof <lionel@thegabas.com>
2024-03-02 12:16:48 -0500

    fix sdl3_image, sdl3_ttf, and sdl3_mixer

--- packages/l/libsdl3_mixer/xmake.lua
+++ packages/l/libsdl3_mixer/xmake.lua
@@ -2,7 +2,7 @@ package("libsdl3_mixer")
     set_license("zlib")
 
     add_urls("https://github.com/libsdl-org/SDL_mixer.git")
-    add_versions("20240224", "ef5373696f3a3bfd90e98be0a145196b7118a665")
+    add_versions("20240302", "839be00b59293c52aadbf5ef1028041acb89476b")
 
     add_deps("cmake")
 
